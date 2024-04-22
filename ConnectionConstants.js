cbdacafee4aaa583f1cc9eb76389d0fb4c86bcc7
olivia1117 <oadamic@gmail.com>
2024-04-22 12:08:22 -0500

    pushing new connection string uri

--- ConnectionConstants.js
+++ ConnectionConstants.js
@@ -1,8 +1,10 @@
-const uri = "[[YOUR-CONNECTION-STRING-HERE]]"	
+const uri = "mongodb+srv://KrabbyPatties:WhoLiv3sInAPin3appl3%3F@syllabyedb.pdubk1g.mongodb.net/?retryWrites=true&w=majority&appName=SyllaByeDB"	
 // Example uri:
 // const uri = "mongodb+srv://testMongoDBUserName:8L4kkR8KszHZTI7S@cluster0.fei8p8f.mongodb.net/?retryWrites=true&w=majority"	
+// password: WhoLiv3sInAPin3appl3?
 
-const databaseName = "movieDatabase"
-const collectionName = "movies"
+
+const databaseName = "syllabiDatabase"
+const collectionName = "syllabi"
 
 module.exports = { uri, databaseName, collectionName };
